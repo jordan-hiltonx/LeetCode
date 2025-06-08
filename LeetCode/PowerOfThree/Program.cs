251b84853511a756f345820946b8295d70d2539a
jordan-hiltonx <jordan-hiltonx@users.noreply.github.com>
2025-06-08 18:02:38 -0400

    power of three

---
+++ LeetCode/PowerOfThree/Program.cs
@@ -0,0 +1,13 @@
+﻿// https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/description/
+
+bool CheckPowersOfThree(int n) {
+    while (n > 0) {
+        if (n % 3 == 2) {
+            return false; // If n has a digit '2' in base 3, it cannot be expressed as a sum of powers of three
+        }
+        n /= 3; // Divide n by 3 to check the next digit
+    }
+    return true; // If we finish the loop without finding a '2', it can be expressed as a sum of powers of three
+}
+
+Console.WriteLine(CheckPowersOfThree((21)));
